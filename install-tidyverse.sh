0c8dd572c35b2e112b97916f2a1c0abdd9dcd935
Oscar de León <odeleongt@gmail.com>
2017-04-17 17:42:28 -0600

    Install tidyverse

---
+++ install-tidyverse.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+sudo apt-get update
+
+sudo apt-get -y --no-install-recommends install libxml2-dev libcairo2-dev libsqlite-dev libmariadbd-dev libmariadb-client-lgpl-dev libpq-dev
+
+. /etc/environment
+
+Rscript -e "install.packages(c('devtools'), repo = 'https://cran.rstudio.com/')"
+Rscript -e "install.packages(c('tidyverse', 'dplyr', 'ggplot2', 'profvis', 'formatR', 'remotes'), repo = 'https://cran.rstudio.com/')"
