91464db80c7b1cd32569fdf28e1b2ce69c128d73
Oscar de León <odeleongt@gmail.com>
2017-04-19 09:44:42 -0600

    Install gitg and add git-flow

---
+++ .improve-git-workflow.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+sudo apt-get update
+
+sudo apt-get -y --no-install-recommends install curl gitg
+
+
+## Install git flow
+curl -OL https://raw.github.com/nvie/gitflow/develop/contrib/gitflow-installer.sh
+chmod +x gitflow-installer.sh
+sudo ./gitflow-installer.sh
+sudo rm -rf gitflow
+rm gitflow-installer.sh
+
