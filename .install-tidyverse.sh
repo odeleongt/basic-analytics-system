2b8481126a8d909c329003ee56843633f3146617
Oscar de León <odeleongt@gmail.com>
2017-04-19 09:49:37 -0600

    Add dbplyr

--- .install-tidyverse.sh
+++ .install-tidyverse.sh
@@ -7,4 +7,4 @@ sudo apt-get -y --no-install-recommends install libxml2-dev libcairo2-dev libsql
 . /etc/environment
 
 Rscript -e "install.packages(c('devtools'), repo = 'https://cran.rstudio.com/')"
-Rscript -e "install.packages(c('tidyverse', 'dplyr', 'ggplot2', 'profvis', 'formatR', 'remotes'), repo = 'https://cran.rstudio.com/')"
+Rscript -e "install.packages(c('tidyverse', 'dplyr', 'dbplyr', 'ggplot2', 'profvis', 'formatR', 'remotes'), repo = 'https://cran.rstudio.com/')"
