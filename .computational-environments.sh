7c6aba52001ff56ad4b7a7a1b228b2e3bdee0076
Oscar de León <odeleongt@gmail.com>
2017-04-20 09:10:17 -0600

    Install Docker

--- .computational-environments.sh
+++ .computational-environments.sh
@@ -14,3 +14,26 @@ sudo apt-get update
 sudo apt-get -y --no-install-recommends  install virtualbox-5.1
 sudo apt-get -y install dkms
 
+
+################################################################################
+## Install Docker
+################################################################################
+
+## Dependencies
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-commoncurl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+## TODO: Check keys
+apt-key adv --list-public-keys  --with-fingerprint --with-colons Docker Release | grep fpr
+## Should equal
+echo "9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88"
+
+## Set up *stable* repository
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+## Install Community Eddition
+sudo apt-get update
+sudo apt-get -y install docker-ce
+
+## Verify that Docker CE or Docker EE is installed correctly by running the hello-world image.
+sudo docker run hello-world
+
