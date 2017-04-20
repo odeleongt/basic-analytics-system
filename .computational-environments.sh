d40b5674d72f8db83ebe8c7fc6e3143cf8faf162
Oscar de León <odeleongt@gmail.com>
2017-04-20 08:50:31 -0600

    Improve documentation

--- .computational-environments.sh
+++ .computational-environments.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
-
+################################################################################
 ## Install VirtualBox
+################################################################################
+
+## Setup repository
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 sudo add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian xenial contrib"
 
+## Install
 sudo apt-get update
 sudo apt-get -y --no-install-recommends  install virtualbox-5.1
 sudo apt-get -y install dkms
