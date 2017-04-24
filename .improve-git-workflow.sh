704ef3807516bd307aa26ad27efb5d62aa9a5fe0
Oscar de León <odeleongt@gmail.com>
2017-04-24 14:56:01 -0600

    Add alias to show branch description

--- .improve-git-workflow.sh
+++ .improve-git-workflow.sh
@@ -15,3 +15,7 @@ rm gitflow-installer.sh
 ## Setup gedit as commit editor
 git config --global core.editor "gedit -s"
 
+## Add alias to show branch description
+## http://stackoverflow.com/a/39867674/2089695
+git config --global --add alias.about '!describe() { git config branch."$(git symbolic-ref --short -q HEAD)".description; }; describe'
+
