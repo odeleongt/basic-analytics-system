f26a236ac76e7b30f16cc4093267c77cf5e1b416
Oscar de León <odeleongt@gmail.com>
2021-04-25 21:10:33 -0600

    Explicitly sudo commands

--- .add-r.sh
+++ .add-r.sh
@@ -19,11 +19,11 @@ mkdir -p $R_HOME
 ## R dependencies
 ################################################################################
 
-apt-get update
+sudo apt-get update
 
-apt-get install -y --no-install-recommends synaptic bash-completion ca-certificates file fonts-texgyre g++ gfortran gsfonts libbz2-1.0 libcurl4 libicu-dev libopenblas-dev libpangocairo-1.0-0 libpcre3 libpng16-16 libtiff5 liblzma5 locales make unzip zip zlib1g
+sudo apt-get install -y --no-install-recommends synaptic bash-completion ca-certificates file fonts-texgyre g++ gfortran gsfonts libbz2-1.0 libcurl4 libicu-dev libopenblas-dev libpangocairo-1.0-0 libpcre3 libpng16-16 libtiff5 liblzma5 locales make unzip zip zlib1g
 
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
 
 locale-gen en_US.utf8
 
@@ -31,7 +31,7 @@ locale-gen en_US.utf8
 
 BUILDDEPS="curl default-jdk libbz2-dev libcairo2-dev libcurl4-openssl-dev libpango1.0-dev libjpeg-dev libicu-dev libpcre3-dev libpng-dev libreadline-dev libtiff5-dev liblzma-dev libx11-dev libxt-dev perl tcl8.6-dev tk8.6-dev texinfo texlive-extra-utils texlive-fonts-recommended texlive-fonts-extra texlive-latex-recommended x11proto-core-dev xauth xfonts-base xvfb zlib1g-dev"
 
-apt-get install -y --no-install-recommends $BUILDDEPS
+sudo apt-get install -y --no-install-recommends $BUILDDEPS
 
 
 ################################################################################
@@ -56,17 +56,17 @@ R_PAPERSIZE=letter R_BATCHSAVE="--no-save --no-restore" R_BROWSER=xdg-open PAGER
 ## Build and install
 make -j 8
 
-make install
+sudo make install
 
 
 ## Add a default CRAN mirror
-echo "options(repos = c(CRAN = 'https://cran.rstudio.com/'), download.file.method = 'libcurl')" >> /usr/local/lib/R/etc/Rprofile.site
+echo "options(repos = c(CRAN = 'https://cran.rstudio.com/'), download.file.method = 'libcurl')" | sudo tee -a /usr/local/lib/R/etc/Rprofile.site
 
 
 ## Fix library path
-echo "R_LIBS_USER='$R_HOME'" >> /usr/local/lib/R/etc/Renviron
+echo "R_LIBS_USER='$R_HOME'" | sudo tee -a /usr/local/lib/R/etc/Renviron
 
-echo "R_LIBS=\${R_LIBS-'$R_HOME:/usr/local/lib/R/library:/usr/lib/R/library'}" >> /usr/local/lib/R/etc/Renviron
+echo "R_LIBS=\${R_LIBS-'$R_HOME:/usr/local/lib/R/library:/usr/lib/R/library'}" | sudo tee -a /usr/local/lib/R/etc/Renviron
 
 ## Use littler installation scripts
 Rscript -e "install.packages(c('littler', 'docopt'), repo = 'https://cran.rstudio.com/')"
@@ -82,14 +82,14 @@ cd ../..
 
 rm -rf tmp
 
-apt-get autoremove -y
+sudo apt-get autoremove -y
 
-apt-get autoclean -y
+sudo apt-get autoclean -y
 
 rm -rf /var/lib/apt/lists/*
 
 
 ## Change ownership for installed packages
-chmod g+wx $R_HOME
-chown $SUDO_USER:$SUDO_USER -R $HOME/R
+sudo chmod g+wx $R_HOME
+sudo chown $SUDO_USER:$SUDO_USER -R $HOME/R
 
